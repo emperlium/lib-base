42aabd54e0f7888da031d820de018f3d0657b617
Nick Peskett <github.perl@peskett.co.uk>
2018-03-16 11:10:15 +0000

    Add docs to Nick::HandleBinEnvDir.

--- lib/Nick/HandleBinEnvDir.pm
+++ lib/Nick/HandleBinEnvDir.pm
@@ -3,6 +3,34 @@ package Nick::HandleBinEnvDir;
 use strict;
 use warnings;
 
+=pod
+
+=head1 NAME
+
+Nick::HandleBinEnvDir - Current library search path management.
+
+=head1 SYNOPSIS
+
+    use Nick::HandleBinEnvDir;
+
+    BEGIN {
+        Nick::HandleBinEnvDir -> handle( 'APP_HOME' );
+    }
+
+=head1 METHODS
+
+=head2 handle()
+
+Adds directory* + /lib to the library search path.
+
+*If an environment variable exists with the name supplied as an argument (typically the application home), otherwise the value returned by get().
+
+=head2 get()
+
+Tries to work out the application home based on the script path.
+
+=cut
+
 sub handle {
     my( $class, $name ) = @_;
     if ( exists $ENV{$name} ) {
